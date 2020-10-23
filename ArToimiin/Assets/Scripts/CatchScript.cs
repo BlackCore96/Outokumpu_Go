fb6f6916a2e89920c08a4d2b2a60afd4debc41e4
veemtee <55981900+veemtee@users.noreply.github.com>
2020-10-23 13:47:10 +0300

    Start

---
+++ ArToimiin/Assets/Scripts/CatchScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatchScript : MonoBehaviour
+{
+    public GameObject hahmo;
+    public GameObject hahmo2;
+    RaycastHit hit;
+
+    public void Button()
+    {
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit))
+        {
+            if (hit.collider.gameObject.Equals(hahmo))
+            
+            {
+                Destroy(hit.collider.gameObject);
+            }
+
+            if (hit.collider.gameObject.Equals(hahmo2))
+
+            {
+                Destroy(hit.collider.gameObject);
+            }
+        }
+    }
+}
---
+++ ArToimiin/Assets/Scripts/UccoMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UccoMovement : MonoBehaviour
+{
+    bool moveRight = true;
+    public float movementSpeed;
+    // Update is called once per frame
+    void Update()
+    {
+        if (moveRight)
+        {
+            transform.Translate(transform.right * movementSpeed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, Vector3.zero) >= 3)
+            {
+                moveRight = false;
+            }
+        }
+        else
+        {
+            transform.Translate(-transform.right * movementSpeed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, Vector3.zero) >= 3)
+            {
+                moveRight = true;
+            }
+        }
+    }
+}
