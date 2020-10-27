e3a7a35afe3a96340cffea2944929b66a449cc93
BlackCore96 <veeti.kaasinen@gmail.com>
2020-10-27 14:44:54 +0200

    NavMesh toimii

--- ArToimiin/Assets/Scripts/UccoMovement.cs
+++ ArToimiin/Assets/Scripts/UccoMovement.cs
@@ -1,29 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class UccoMovement : MonoBehaviour
 {
-    bool moveRight = true;
     public float movementSpeed;
-    // Update is called once per frame
-    void Update()
+    private NavMeshSurface navMeshSurface;
+    private NavMeshAgent navMeshAgent;
+
+    private void Start()
     {
-        if (moveRight)
+        navMeshSurface = FindObjectOfType<NavMeshSurface>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        SetDestination();
+    }
+
+    private void Update()
+    {
+        if (!navMeshAgent.hasPath)
         {
-            transform.Translate(transform.right * movementSpeed * Time.deltaTime);
-            if (Vector3.Distance(transform.position, Vector3.zero) >= 3)
-            {
-                moveRight = false;
-            }
-        }
-        else
-        {
-            transform.Translate(-transform.right * movementSpeed * Time.deltaTime);
-            if (Vector3.Distance(transform.position, Vector3.zero) >= 3)
-            {
-                moveRight = true;
-            }
+            SetDestination();
         }
     }
+
+    void SetDestination()
+    {
+        navMeshAgent.SetDestination(navMeshSurface.transform.position + (Vector3.forward * Random.Range(-4f, 4f)) + (Vector3.right * Random.Range(-4f, 4f)));
+    }
+
 }
