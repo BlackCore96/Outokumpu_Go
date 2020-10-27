bdd0d1db709b7552ef622d26eadae4514be8070f
BlackCore96 <veeti.kaasinen@gmail.com>
2020-10-27 13:26:55 +0200

    update

--- ArToimiin/Assets/Scripts/GroundScan.cs
+++ ArToimiin/Assets/Scripts/GroundScan.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 using UnityEngine.Experimental.XR;
@@ -10,6 +11,8 @@ public class GroundScan : MonoBehaviour
 {
     private ARSessionOrigin arOrigin;
     private ARRaycastManager arRayCastManager;
+    private NavMeshSurface navMeshSurface;
+    private new Camera camera;
     Vector3 screenCenter;
     List<ARRaycastHit> hits = new List<ARRaycastHit>();
 
@@ -18,11 +21,26 @@ public class GroundScan : MonoBehaviour
     {
         arOrigin = FindObjectOfType<ARSessionOrigin>();
         arRayCastManager = FindObjectOfType<ARRaycastManager>();
-        screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(.5f, .5f));
+        try
+        {
+            navMeshSurface = GameObject.FindGameObjectWithTag("ARPlane").GetComponent<NavMeshSurface>();
+        }
+        catch
+        {
+            Invoke("LateStart", .05f);
+        }
+        camera = arOrigin.GetComponentInChildren<Camera>();
+        screenCenter = camera.ViewportToScreenPoint(new Vector3(.5f, .5f));
+        InvokeRepeating("UpdateNavMesh", .5f, 3);
     }
 
-    void ScanGround()
+    void LateStart()
     {
-        arRayCastManager.Raycast(screenCenter, hits, TrackableType.Planes);
+        navMeshSurface = GameObject.FindGameObjectWithTag("ARPlane").GetComponent<NavMeshSurface>();
+    }
+
+    void UpdateNavMesh()
+    {
+        navMeshSurface.BuildNavMesh();
     }
 }
