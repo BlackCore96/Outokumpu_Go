bd55d532f45de9ceda402b215f4b1a876523c1cd
BlackCore96 <veeti.kaasinen@gmail.com>
2020-10-27 11:06:12 +0200

    update

---
+++ ArToimiin/Assets/Scripts/GroundScan.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.Experimental.XR;
+using System;
+
+public class GroundScan : MonoBehaviour
+{
+    private ARSessionOrigin arOrigin;
+    private ARRaycastManager arRayCastManager;
+    Vector3 screenCenter;
+    List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+
+    void Start()
+    {
+        arOrigin = FindObjectOfType<ARSessionOrigin>();
+        arRayCastManager = FindObjectOfType<ARRaycastManager>();
+        screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(.5f, .5f));
+    }
+
+    void ScanGround()
+    {
+        arRayCastManager.Raycast(screenCenter, hits, TrackableType.Planes);
+    }
+}
