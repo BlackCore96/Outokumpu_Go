97580b63b67a3eeec7698c501acbcbf3e8ecd0ba
BlackCore96 <veeti.kaasinen@gmail.com>
2020-11-05 11:10:07 +0200

    maps toimii

--- ArToimiin/Assets/Scripts/SpawnWorldObjects.cs
+++ ArToimiin/Assets/Scripts/SpawnWorldObjects.cs
@@ -8,15 +8,19 @@ using UnityEngine;
 public class SpawnWorldObjects : MonoBehaviour
 {
     public GameObject stopPrefab;
-    Coordinates coordinates = new Coordinates(62.725284000000, 29.018918000000, 0);
+    Coordinates coordinates = new Coordinates(62.732301, 29.054945, 0);
+    Vector3 position;
+    GameObject stop;
 
     private void Start()
     {
-        GameObject stop = Instantiate(stopPrefab);
-        Vector3 position = coordinates.convertCoordinateToVector(0);
-        Debug.Log(Coordinates.convertVectorToCoordinates(position).latitude.ToString() + " / " + Coordinates.convertVectorToCoordinates(position).longitude.ToString());
-        Debug.Log(coordinates.latitude.ToString() + " / " + coordinates.longitude.ToString());
-        Debug.Log(position.x.ToString() + " / " + position.z.ToString());
-        stop.transform.localPosition = position;
+        Invoke("LateStart", .5f);     
+    }
+
+    void LateStart()
+    {
+        stop = Instantiate(stopPrefab);
+        position = coordinates.convertCoordinateToVector();
+        stop.transform.position = position;
     }
 }
