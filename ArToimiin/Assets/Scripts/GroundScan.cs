42ab494c06d380ec14c2e3c09500bded2a12bf14
BlackCore96 <veeti.kaasinen@gmail.com>
2020-11-10 12:27:17 +0200

    Revert "??"
    This reverts commit 0c6eda222e83bb34efbcf89f84740925ab0bd9b1.

--- ArToimiin/Assets/Scripts/GroundScan.cs
+++ ArToimiin/Assets/Scripts/GroundScan.cs
@@ -10,11 +10,19 @@ using System;
 
 public class GroundScan : MonoBehaviour
 {
+    public Vector3 meshBounds;
+    private GameObject navMeshObject;
+    [Space, Header("Size in m^2 until scanning is complete")]
+    public float desiredSize;
+    [Space]
+    public float meshSize;
+    [Space]
     public Text debugLogText;
     public GameObject prefabCharacter;
     public GameObject prefabMuna;
     private ARSessionOrigin arOrigin;
     private ARRaycastManager arRayCastManager;
+    private MeshFilter navMesh;
     private NavMeshSurface navMeshSurface;
     public new Camera camera;
     bool navMeshIsActive;
@@ -30,37 +38,46 @@ public class GroundScan : MonoBehaviour
         camera = arOrigin.GetComponentInChildren<Camera>();
         screenCenter = camera.ViewportToScreenPoint(new Vector3(.5f, .5f));
         InvokeRepeating("UpdateNavMesh", .5f, .5f);
-        InvokeRepeating("SpawnCharacter", .6f, .5f);
     }
 
     void UpdateNavMesh()
     {
-        try
+        if (!navMeshIsActive)
         {
-            navMeshSurface.BuildNavMesh();
-        }
-        catch
-        {
-            Debug.Log("Searching NavMesh...");
-            debugLogText.text = "Searching NavMesh...";
-            navMeshSurface = FindObjectOfType<NavMeshSurface>();
             try
             {
                 navMeshSurface.BuildNavMesh();
-                Debug.Log("NavMesh Found!");
-                debugLogText.text = "NavMesh Found!";
-                navMeshIsActive = true;
+                meshBounds = new Vector3(navMesh.mesh.bounds.size.x * navMeshObject.transform.localScale.x, 1, navMesh.mesh.bounds.size.z * navMeshObject.transform.localScale.z);
+                meshSize = meshBounds.x * meshBounds.z;
             }
-            catch { }
+            catch
+            {
+                Debug.Log("Searching NavMesh...");
+                debugLogText.text = "Searching NavMesh...";
+                navMeshSurface = FindObjectOfType<NavMeshSurface>();
+                try
+                {
+                    navMeshSurface.BuildNavMesh();
+                    navMeshObject = navMeshSurface.gameObject;
+                    navMesh = navMeshObject.GetComponent<MeshFilter>();
+                    Debug.Log("NavMesh Found!");
+                    debugLogText.text += "NavMesh Found!";
+                }
+                catch { }
+            }
+
+            if (meshSize >= desiredSize)
+            {
+                SpawnCharacter();
+            }
         }
     }
 
     void SpawnCharacter()
     {
-        if (navMeshIsActive)
-        {
-            GameObject muna = Instantiate(prefabMuna, navMeshSurface.transform.position, Quaternion.identity);
-            CancelInvoke("SpawnCharacter");
-        }
+        Debug.Log("Spawned the egg!");
+        debugLogText.text = "Spawned the egg!";
+        navMeshIsActive = true;
+        GameObject muna = Instantiate(prefabMuna, navMeshSurface.transform.position, Quaternion.identity);
     }
 }
