1d5c4988a78eeefc0a1678ee0e44a7bdc20161eb
veemtee <55981900+veemtee@users.noreply.github.com>
2020-11-18 12:49:13 +0200

    1249

---
+++ ArToimiin/Assets/Scripts/ParticleContainer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleContainer : MonoBehaviour
+{
+    public ParticleSystem catchParticle;
+    public ParticleSystem runParticle;
+    
+
+}
