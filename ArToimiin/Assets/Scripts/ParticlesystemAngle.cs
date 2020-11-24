f1b65cae1320244a21b279a53b5db36a0b0dfcdf
veemtee <55981900+veemtee@users.noreply.github.com>
2020-11-24 13:37:00 +0200

    Jotain

--- ArToimiin/Assets/Scripts/ParticlesystemAngle.cs
+++ ArToimiin/Assets/Scripts/ParticlesystemAngle.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class ParticlesystemAngle : MonoBehaviour
 {
-    public GameObject target;
+    public Transform target;
     public Transform pSystem;
 
     // Start is called before the first frame update
     void Start()
     {
-        target = GameObject.FindWithTag("ParticleTarget");
+        target = FindObjectOfType<Camera>().transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 directionToTarget = (target.transform.position - pSystem.position).normalized;
+        Vector3 directionToTarget = (target.position - pSystem.position).normalized;
         Quaternion rottation = Quaternion.LookRotation(directionToTarget, Vector3.forward);
         transform.rotation = rottation;
     }
