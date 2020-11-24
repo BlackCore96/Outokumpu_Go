ec5f6ac464812f6ef1b8c201f76813582e69dce4
veemtee <55981900+veemtee@users.noreply.github.com>
2020-11-24 10:45:03 +0200

    2411TraktoriSäde

---
+++ ArToimiin/Assets/Scripts/ParticleEmissionStrenght.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEmissionStrenght : MonoBehaviour
+{
+    public ParticleSystem ps;
+    public float strenght;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var em = ps.emission;
+        em.enabled = true;
+
+        em.rateOverTime = 1 * strenght;
+    }
+}
---
+++ ArToimiin/Assets/Scripts/ParticlesystemAngle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlesystemAngle : MonoBehaviour
+{
+    public GameObject target;
+    public Transform pSystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindWithTag("ParticleTarget");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 directionToTarget = (target.transform.position - pSystem.position).normalized;
+        Quaternion rottation = Quaternion.LookRotation(directionToTarget, Vector3.forward);
+        transform.rotation = rottation;
+    }
+}
