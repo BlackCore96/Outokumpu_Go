539e24dca0bbb54be2c27acd81c718768e53d44e
BlackCore96 <veeti.kaasinen@gmail.com>
2020-12-02 10:12:35 +0200

    ..

---
+++ ArToimiin/Assets/Scripts/HelpperiScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class HelpperiScript : MonoBehaviour
+{
+    CanvasGroup canvasGroup;
+    Text text;
+    public float typeSpeed;
+
+    [Header("Helpperi tekstit")]
+    public string[] allCharactersCollected;
+    public string[] firstLaunch;
+    public string[] firstMinigame;
+    public string[] error;
+
+    public enum HelperText
+    {
+        FIRST_LAUNCH,
+        FIRST_MINIGAME,
+        ALL_CHARACTERS_COLLECTED
+    }
+
+    public string[] GetHelperText(HelperText helper)
+    {
+        switch (helper)
+        {
+            case HelperText.ALL_CHARACTERS_COLLECTED:
+                return allCharactersCollected;
+            case HelperText.FIRST_LAUNCH:
+                return firstLaunch;
+            case HelperText.FIRST_MINIGAME:
+                return firstMinigame;
+            default:
+                return error;
+        }
+    }
+
+    void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        text = GetComponentInChildren<Text>();
+    }
+
+    IEnumerator WriteText(string s)
+    {
+        text.text = "";
+        int i = 0;
+        foreach (char character in s)
+        {
+            text.text.Insert(i, character.ToString());
+            i++;
+            yield return new WaitForSeconds(typeSpeed);
+        }
+    }
+}
